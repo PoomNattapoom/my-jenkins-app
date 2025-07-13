d9202b14df5736e7f34985fd9cb6c8b8e5805fcc
PoomNattapoom <rockyaggi@gmail.com>
2025-07-13 15:57:49 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("🎉 Hello from Jenkins-powered Node.js App!");
+});
+
+app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
